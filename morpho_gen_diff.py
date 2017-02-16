3477672f5922109d114a70791955fe92dc18deb9
Sherdil <sniyaz@berkeley.edu>
2017-02-16 13:29:24 -0800

    Whoops, last commit forgot the actual diff sript!

---
+++ morpho_gen_diff.py
@@ -0,0 +1,42 @@
+import sys
+import pickle
+import os
+from bpe import apply_merge_ops, delimit_corpus, get_vocabulary, apply_presegs, segment_vocab, recover_preseg_boundary, write_segmentation_list
+from copy import deepcopy
+from collections import Counter
+
+import pdb
+
+"""Compare our segmentations with those from a BPE implementation"""
+
+if __name__ == '__main__':
+
+    corpus_file = sys.argv[1]
+    presegs_file = sys.argv[2]
+    output_name = sys.argv[3]
+
+    #Load in presegs
+    presegs = open(presegs_file, "rb")
+    presegs = pickle.load(presegs)
+    
+    #Apply the presegs to morpho_file
+    corpus = open(corpus_file, "r")
+    morpho_vocab = get_vocabulary(corpus)
+    morpho_preseg = apply_presegs(morpho_vocab, presegs)
+
+    #TODO: Avoid hard-coding
+    num_iters = 32000
+    #Segment morpho version
+    morpho_segmentations, morpho_operations = segment_vocab(morpho_preseg, num_iters, use_eol=False)
+
+    #Recover segmentations of training corpora and write then out
+    final_morpho_seg = recover_preseg_boundary(morpho_vocab, presegs, morpho_segmentations)
+    write_segmentation_list(output_name + "_morpho", morpho_vocab, final_morpho_seg)
+
+    #Now...do the baseline!
+    base_vocab = get_vocabulary(corpus)
+    corpus.close()
+    base_segmentations, base_operations = segment_vocab(base_vocab, num_iters, use_eol=True)
+    write_segmentation_list(output_name + "_base", base_vocab, base_segmentations)
+
+   
